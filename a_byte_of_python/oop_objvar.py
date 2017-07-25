39cfba707872399e52247817e627e0ec61431f36
Cloudtree11 <yunkun233@gmail.com>
2017-07-25 19:30:14 +0800

    Add class demo cases

---
+++ a_byte_of_python/oop_objvar.py
@@ -0,0 +1,56 @@
+
+# coding=UTF-8
+
+class Robot:
+    """ A robot with name  """
+    population = 0
+
+    def __init__(self, name):
+        """Initialazation"""
+        self.name = name
+        print '(Initializing {})'.format(self.name)
+
+        Robot.population += 1
+
+    def die(self):
+        """I'm dead."""
+        print "{} is being destroyed!".format(self.name)
+
+        Robot.population -= 1
+
+        if Robot.population == 0:
+            print "{} was the last one.".format(self.name)
+        else:
+            print "There are still {:d} robots working.".format(
+                    Robot.population)
+
+    def say_hi(self):
+        """
+
+        """
+        print "Greetings, my masters call me {}.".format(self.name)
+
+    @classmethod
+    def how_many(cls):
+        """Print the population numbers."""
+        print "We have {:d} robots.".format(cls.population)
+
+
+droid1 = Robot("R2-D2")
+droid1.say_hi()
+Robot.how_many()
+
+droid2 = Robot("C-3PO")
+droid2.say_hi()
+Robot.how_many()
+
+print "\nRobots can do some work here.\n"
+
+print "Robots have finished their work. So let's destroy them."
+droid1.die()
+droid2.die()
+
+Robot.how_many()
+
+
+
---
+++ toolbox/cal.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from datetime import *
+
+base = 18500
+dt_begin = datetime.strptime("2016-07-01", "%Y-%m-%d")
+# 2017-07-05 +1000
+dt_end = datetime.strptime("2017-07-05", "%Y-%m-%d")
+day_count = (dt_end - dt_begin).days
+day_rate = 0.04 / 365
+total = base
+
+for i in range(day_count):
+    total = total + total * day_rate
+
+dt_now = datetime.now()
+total += 1000
+day_count = (dt_now - dt_end).days
+
+for i in range(day_count):
+    total = total + total * day_rate
+
+
+print "day count is", day_count
+print "sum is %.2f" % total
