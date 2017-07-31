c8b06f2390743b0443913d847e97b670c61028b5
Cloudtree11 <yunkun233@gmail.com>
2017-07-31 19:49:01 +0800

    Update crawler demo

--- crawler/demo.py
+++ crawler/demo.py
@@ -1,37 +1,71 @@
 
 # coding=UTF-8
 
-import urllib2
-import cookielib
+import urllib2, cookielib, re, time
 
-'''response = urllib2.urlopen('http://www.baidu.com')
+def get_http_response(url):
+    '''Get http response method
 
-print response.headers
+    '''
 
-html = response.read()
-f = open('content.html', 'w')
-f.write(html)
-f.close()'''
+    # First method
+    #response1 = urllib2.urlopen(url)
+    #print response1.getcode()
+    #print len(response1.read())
 
-url = 'http://www.baidu.com'
+    # Second method
+    request = urllib2.Request(url)
+    request.add_header("user-agent", "Mozilla/5.0")
+    response = urllib2.urlopen(request)
+    #print response2.getcode()
+    #print len(response2.read())'''
 
-response1 = urllib2.urlopen(url)
-print response1.getcode()
-print len(response1.read())
+    # Third method
+    #cj = cookielib.CookieJar()
+    #opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    #urllib2.install_opener(opener)
+    #response = urllib2.urlopen(request)
+    #print "Http return code:", response3.getcode()
+    #print "Actual url:", response3.geturl()
+    #print "Http return info:\n",response3.info()
+    #print "Cookies:\n", cj
+    return response
 
-request = urllib2.Request(url)
-request.add_header("user-agent", "Mozilla/5.0")
-response2 = urllib2.urlopen(request)
-print response2.getcode()
-print len(response2.read())
+url = 'http://blog.csdn.net/dog250?viewmode=contents'
+http_response = get_http_response(url)
+html_raw = http_response.read()
+#f = open('content.html', 'w')
+#f.write(html_raw)
+#f.close()
+#html_regex = re.compile(r"<a href=\"/dog250/article/details/\d+\">")
+html_regex = re.compile(r"\"/dog250/article/details/\d+\">")
+links = []
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
-response3 = urllib2.urlopen(request)
-print response3.getcode()
-print response3.geturl()
-print response3.info()
-print cj
-print len(response3.read())
+for link in html_regex.findall(html_raw):
+    link = link[1:-2]
+    link = "http://blog.csdn.net" + link
+    #print link
+    links.append(link)
+
+for link in links:
+    time.sleep(10)
+    http_response = get_http_response(link)
+    html_raw = http_response.read()
+    title_regex = re.compile(r"<title>.+?Netfi", re.S)
+    title_match = title_regex.search(html_raw)
+    if title_match == None:
+        print "title match failed"
+    else:
+        title = title_match.group()
+        title = title[7:-8]
+    #with open(file_name, 'w') as f:
+    #    f.write(html_raw)
+        print title
+
+
+
+
+
+
+
 
